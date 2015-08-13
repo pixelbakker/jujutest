1a4d33d7496d758fa3a5fe48b404e8d011d19f15
Leif Arne Vian Bakker <leifarnebakker1@Leifs-MacBook-Pro.local>
2015-08-13 12:50:35 +0200

    sdf

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+var config = require('./config/config')
 var http = require('http');
+
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(3000, 'p1');
+}).listen(config.listen_port);
